11d9ed1f6ae1604201777af54c7375927eaf18dd
Danielitosk <d.santiago07@hotmail.com>
2024-10-22 15:20:22 -0600

    cars cars cars
    sadness

--- Cars cars cars/sketch.js
+++ Cars cars cars/sketch.js
@@ -1,7 +1,7 @@
 // Cars cars cars
 // Daniel
 // 18 Oct
-
+// A program that displays cars and trucks in a road > <
 let car;
 let truck;
 
@@ -15,7 +15,7 @@ function setup() {
 function draw() {
   background(220);
   drawRoad();
-  car.display();
+  drawCar();
 }
 
 //function for making the road
@@ -35,50 +35,54 @@ function drawRoad() {
 
 //
 class Vehicle {
-  constructor(type,speed){
-    this.x = random(0,width);
-    this.y= random(0,1);
-    this.type = type;
-    this.color = color(random(0,255));
-    this.direction = random(0,1);
+  constructor(x,y,type,dir,speed){
+    
+    this.x = x;
+    this.y= y;
+    this.type = int(random(2));
+    this.color = color(random(0,255),random(0,255),random(0,255));
+    this.dir = dir;
     this.xSpeed = speed;
   }
-
-  drawcar(){
+  // making the car
+  drawCar(){
+    rectMode(CENTER);
+    fill(this.color);
     rect(this.x,this.y,20,40);
     rect(this.x-10, this.y-10,5,5);
     rect(this.x+10,this.y-10,5,5);
     rect(this.x-10, this.y+10,5,5);
     rect(this.x+10, this.y+10,5,5);  
   }
-
+  // making the truck
+  drawTruck(){
+    rectMode(CENTER);
+    fill(this.color);
+    rect(this.x,this.y,20,20);
+    rect(this.x+20,this.y,20,40);
+  }
   
   display() {
-    //render cars to the screen
-    rectMode(CENTER);
-    switch (this.type){
-    case car:
-      fill(this.color);
-      rect(this.x,this.y,20,40);
-      fill(200)
-      rect(this.x-10, this.y-10,5,5);
-      rect(this.x+10,this.y-10,5,5);
-      rect(this.x-10, this.y+10,5,5);
-      rect(this.x+10, this.y+10,5,5);
-      break;
-
-    case truck:
-      fill(this.color);
-      rect(this.x,this.y,20,20);
-      rect(this.x+20,this.y,20,40);
-
+    //render cars and trucks to the screen
+    if (this.type===0){  // 0-display car
+      this.drawCar();
     }
+    else if (this.type===1){ // 1-display truck
+
+      this.drawTruck();
+    }  
+   
       
-    
   }
 
+  // move function
+  move(){
+    this.x += this.xSpeed;
+    if (0> this.x){
+      this.x
+    }
 
-
+  }
 }
 
 

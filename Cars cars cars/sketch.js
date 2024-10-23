d46b55027af1a277abfd29ae3b1a9af4378f206f
Danielitosk <d.santiago07@hotmail.com>
2024-10-23 15:19:33 -0600

    cars cars cars
    i did two cars

--- Cars cars cars/sketch.js
+++ Cars cars cars/sketch.js
@@ -4,23 +4,29 @@
 // A program that displays cars and trucks in a road > <
 let car;
 let truck;
+let east = [];
+let west = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  car= new Vehicle(0);
-  truck= new Vehicle(1);
+  car = new Vehicle(400, 500, 2, 3);
+  truck = new Vehicle(800, 600, 1, 6);
 
 }
 
 function draw() {
   background(220);
   drawRoad();
-  drawCar();
+  car.action();   //getting the cars to mpve
+  truck.action();   //gettinf the trucks to move
+  for (let i=0; i< east.length; i++){
+    east[i].action;
+  }
 }
 
 //function for making the road
-let linespacing = 70;  //
-function drawRoad() { 
+let linespacing = 70;  // middle lines separation
+function drawRoad() {
   noStroke();
   fill(10);
   rectMode(CENTER);
@@ -29,61 +35,110 @@ function drawRoad() {
   for (let i = 10; i < width; i += linespacing) {
     fill(250);
     rect(i, height / 2, 30, 5);
-
   }
 }
 
 //
 class Vehicle {
-  constructor(x,y,type,dir,speed){
-    
+  constructor(x, y, dir, speed) {
     this.x = x;
-    this.y= y;
+    this.y = y;
     this.type = int(random(2));
-    this.color = color(random(0,255),random(0,255),random(0,255));
+    this.color = color(random(0, 255), random(0, 255), random(0, 255));
     this.dir = dir;
     this.xSpeed = speed;
+    this.frequency;
   }
   // making the car
-  drawCar(){
+  drawCar() {
     rectMode(CENTER);
     fill(this.color);
-    rect(this.x,this.y,20,40);
-    rect(this.x-10, this.y-10,5,5);
-    rect(this.x+10,this.y-10,5,5);
-    rect(this.x-10, this.y+10,5,5);
-    rect(this.x+10, this.y+10,5,5);  
+    rect(this.x, this.y, 40, 20);
+    rect(this.x - 10, this.y - 10, 5, 5);
+    rect(this.x + 10, this.y - 10, 5, 5);
+    rect(this.x - 10, this.y + 10, 5, 5);
+    rect(this.x + 10, this.y + 10, 5, 5);
   }
   // making the truck
-  drawTruck(){
+  drawTruck() {
     rectMode(CENTER);
     fill(this.color);
-    rect(this.x,this.y,20,20);
-    rect(this.x+20,this.y,20,40);
+    rect(this.x, this.y, 90, 40);
+    rect(this.x + 60, this.y, 20, 40);
   }
-  
+
   display() {
     //render cars and trucks to the screen
-    if (this.type===0){  // 0-display car
+    if (this.type === 0) {  // 0-display car
       this.drawCar();
     }
-    else if (this.type===1){ // 1-display truck
-
+    else if (this.type === 1) { // 1-display truck
       this.drawTruck();
-    }  
-   
-      
+    }
+
+
+  }
+
+  mouseCLicked(){
+    east.push(new Vehicle(400,500,east,8));
   }
 
   // move function
-  move(){
+  move() {
+    
+    if (this.x <= 0) {
+      this.x += width;
+    }
+    if (this.x >= width) {
+      this.x -= width;
+    }
     this.x += this.xSpeed;
-    if (0> this.x){
-      this.x
+
+    if (this.dir===0){
+      this.xSpeed += 1;
+    }
+
+    else if (this.dir===1) {
+      this.xSpeed -= 1;
     }
 
   }
-}
 
+
+
+  // increase velocity
+  speedUp() {
+    if (this.xSpeed < 1) {
+      this.xSpeed += 1;
+    }
+  }
+  //decrease velocity
+  speedDown() {
+    if (this.xSpeed > 30) {
+      this.xSpeed -= 1;
+    }
+  }
+
+  changeColor() {
+    this.color = color(random(0, 255), random(0, 255), random(0, 255));
+  }
+
+  // action
+  action() {
+    this.move();
+    this.frequency = int(random(0, 100));
+    if (this.frequency === 99) {
+      this.changeColor();
+    }
+    else if (this.frequency === 98) {
+      this.speedDown();
+    }
+    else if (this.frequency === 97) {
+      this.speedUp();
+    }
+    this.display();
+  }
+
+}
 
 

b3eb5df2db35b2f64bf545f62a547151fc921dba
Danielitosk <d.santiago07@hotmail.com>
2024-10-24 13:06:40 -0600

    cars cars cars
    did one line of 20 cars

--- Cars cars cars/sketch.js
+++ Cars cars cars/sketch.js
@@ -9,18 +9,23 @@ let west = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  car = new Vehicle(400, 500, 2, 3);
-  truck = new Vehicle(800, 600, 1, 6);
 
+  for (let i = 0; i < 20; i++) {
+    east.push(new Vehicle(random(int(1000)), 500, 0, 10));  //cast 20 cars going to the east
+  }
+  for (let i = 0; i < 20; i++) {
+    west.push(new Vehicle(random(int(1000)), 200, 1, 10));   //cast 20 cars going to the west
+  }
 }
 
 function draw() {
   background(220);
   drawRoad();
-  car.action();   //getting the cars to mpve
-  truck.action();   //gettinf the trucks to move
-  for (let i=0; i< east.length; i++){
-    east[i].action;
+  for (let i = 0; i < east.length; i++) {
+    east[i].action();
+  }
+  for (let i = 0; i<west.length; i++){
+    west[i].action();
   }
 }
 
@@ -36,21 +41,23 @@ function drawRoad() {
     fill(250);
     rect(i, height / 2, 30, 5);
   }
+
 }
 
 //
 class Vehicle {
   constructor(x, y, dir, speed) {
     this.x = x;
-    this.y = y;
+    this.y = random(height / 2 - 171, height / 2 -25);
     this.type = int(random(2));
     this.color = color(random(0, 255), random(0, 255), random(0, 255));
-    this.dir = dir;
+    this.dir = 0;
     this.xSpeed = speed;
     this.frequency;
   }
   // making the car
   drawCar() {
+
     rectMode(CENTER);
     fill(this.color);
     rect(this.x, this.y, 40, 20);
@@ -79,13 +86,11 @@ class Vehicle {
 
   }
 
-  mouseCLicked(){
-    east.push(new Vehicle(400,500,east,8));
-  }
+ 
 
   // move function
   move() {
-    
+
     if (this.x <= 0) {
       this.x += width;
     }
@@ -94,29 +99,31 @@ class Vehicle {
     }
     this.x += this.xSpeed;
 
-    if (this.dir===0){
-      this.xSpeed += 1;
-    }
+    // if (this.dir === 0) {
+    //   this.y = this.y = random(height / 2 - 150, height / 2 + 20);
+    // }
 
-    else if (this.dir===1) {
-      this.xSpeed -= 1;
-    }
+    // else {
+    //   this.y = this.y = random(height / 2 + 250, height / 2 - 10);
+    // }
 
+    if (this.dir === 1) {
+      this.xSpeed = -this.xSpeed;
+    }
+    else {
+      this.xSpeed= this.xSpeed;
+    }
   }
 
 
 
   // increase velocity
   speedUp() {
-    if (this.xSpeed < 1) {
-      this.xSpeed += 1;
-    }
+    this.xSpeed += 1;
   }
   //decrease velocity
   speedDown() {
-    if (this.xSpeed > 30) {
-      this.xSpeed -= 1;
-    }
+    this.xSpeed -= 1;
   }
 
   changeColor() {

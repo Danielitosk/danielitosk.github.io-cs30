4142599e235acd656f56e50181d4296413860ed0
Danielitosk <d.santiago07@hotmail.com>
2024-11-18 14:43:55 -0600

    baloon tree
    done the "user choose leaf depth"

--- Baloon tree/sketch.js
+++ Baloon tree/sketch.js
@@ -4,13 +4,13 @@
 //
 
 let scale = 15;
-
+let leaflayer= 1;
 function setup() {
   createCanvas(500, 500);
   
 }
 function draw() {
-  background(255);
+  background(255);  // call background every frame so the tree updates every frame
   drawTree(width / 2, height * 0.9, 90, 6);
 
 }
@@ -20,7 +20,7 @@ function drawLine(x1, y1, x2, y2, depth) {
 }
 function drawTree(x1, y1, angle, depth) {
 
-  let treeAngle = map(mouseX, 0, width, 3, 13);
+  let treeAngle = map(mouseX, 0, width, 3, 13); // variable for the interactive angles
 
   strokeWeight(1);
   strokeWeight(depth - 0.5);
@@ -31,19 +31,19 @@ function drawTree(x1, y1, angle, depth) {
     let y2 = y1 - sin(radians(angle)) * depth * scale; //using trig ratios. Get shorter based on depth
 
     drawLine(x1, y1, x2, y2, depth);
-
-    if (depth < 5) {
+    
+    if (depth < leaflayer) {  // for not drawing the leaves in the trunk
       drawLeaf(x2, y2, depth * 10);
     }
 
     //for a 3-branch tree:
 
-    drawTree(x2, y2, angle +treeAngle, depth - 1);
+    drawTree(x2, y2, angle +treeAngle, depth - 1);  // if the user swipes right, the tree angle increments
     drawTree(x2, y2, angle, depth - 1);
-    drawTree(x2, y2, angle -treeAngle, depth - 1);
+    drawTree(x2, y2, angle -treeAngle, depth - 1);  // if the user swipes left, the tree angle decreases
   }
 
-
+  // create the balloon leaves
   function drawLeaf(x, y, d) {
     fill(random(0, 255), random(0, 255), random(0, 255), 300);
 
@@ -51,4 +51,13 @@ function drawTree(x1, y1, angle, depth) {
   }
 
 
+}
+
+function keyPressed(){
+  if (keyCode===88){
+    leaflayer= leaflayer-1;
+  }
+  else if( keyCode===90){
+    leaflayer= leaflayer+1;
+  }
 }
